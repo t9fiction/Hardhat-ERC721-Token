04a3b57ec13fd828da5040f7c948991ab0aa0f07
SagitarriusA <sohail.sohailishaq@gmail.com>
2021-09-11 19:48:52 +0500

    Complete without hardhat testing

---
+++ scripts/AXY721-script.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const AXY721 = await hre.ethers.getContractFactory("AXY721");
+  const axy721 = await AXY721.deploy("Hello, Hardhat!");
+
+  await axy721.deployed();
+
+  console.log("AXY721 deployed to:", axy721.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
